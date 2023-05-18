e913b7783d4372ca06fccc4bd6d9d59c6515021b
mohamed <mohamdlb506@gmail.com>
2023-05-19 00:44:05 +0300

    Add project files.

---
+++ Solve.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+void RevNumber(int &number)
+{
+	int retNumber = 0;
+	while (number != 0)
+	{
+		int lastDigit = number % 10;
+		retNumber *= 10; // shift retNumber one digit to set the lastDigit
+		retNumber += lastDigit;
+		number /= 10;// remove the last digit
+	}
+	 number = retNumber;
+}
+int main()
+{
+	int number = 12345;
+	cout << "the number befor reverse : " << number << endl;
+	RevNumber(number);
+	cout << "the number after reverse : " << number << endl;
+
+	return 0;
+}
